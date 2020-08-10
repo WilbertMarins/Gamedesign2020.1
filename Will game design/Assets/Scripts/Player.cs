6f207553a8b3990bcf7d9decad6079885a7f42ed
Wilbert Marins <wilbertluis2014@gmail.com>
2020-08-10 01:22:24 -0400

    add jump -  aula 05

--- Will game design/Assets/Scripts/Player.cs
+++ Will game design/Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ public class Player : MonoBehaviour
 {
     public float Speed;
     public float JumpForce;
+    public bool isJumping;
+    public bool doubleJump;
+
     private Rigidbody2D rigid;
 
     // Start is called before the first frame update
@@ -33,12 +36,48 @@ public class Player : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump"))
         {
-        
+            if (!isJumping)
+            {
                 rigid.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
-           
+                doubleJump = true;
+                
+            }
+            else
+            {
+                if (doubleJump)
+                {
+                    rigid.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+
         }
     }
 
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8) // layer Ground
+        {
+            isJumping = false;
+            //doubleJump = false;
+            //animator.SetBool("jump", false);
+        }
+
+        //if (collision.gameObject.tag == "Spike" || collision.gameObject.tag == "Saw") // tag Spike or Saw
+        //{
+            //GameController.instance.ShowGameOver();
+            //Destroy(gameObject);
+        //}
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8) // layer Ground
+        {
+            isJumping = true;
+        }
+    }
 
 
 
